762b3ba06db027c36574c2181afd2214e2f79e80
Ivan-Jose-Perez <ivan.jose@ivanjp.tech>
2024-01-31 20:33:40 -0800

    Adding a pseudocode for js and html skeleton

---
+++ workshop19.js
@@ -0,0 +1,28 @@
+// Build your website according to this user story:
+
+    // A visitor enters the website and finds a list of available freelancers. Each freelancer has a name, an occupation, and a starting price for their services. They observe on the list Alice, the writer with a starting price of $30, and Bob, who is a teacher, has a starting price of $50.
+
+    // The visitor also finds a message that displays the average starting price of all the freelancers. In this example, the average starting price is $40.
+
+    // A few seconds later, a new listing appears for a freelancer named Carol, who is a programmer and has a starting price of $70. The average starting price is updated to $50.
+
+    // New freelancers continue to appear every few seconds, and the average starting price is updated accordingly.
+
+// What I know:
+    // I have to create a list of freelancers and their starting price for each. 
+    // I have to create a message that displays the average starting price of all the freelancers and it is updated w/ every entry.
+    // my functions will have to focus on updating names and the average price.
+
+// What I don't know:
+    //...
+
+// Guiding Questions:
+    //Is your HTML file connected to your JS file? YES
+    // Have you defined arrays for possible names and occupations? NO
+    // Have you defined an initial array of freelancers?
+    // Do you know how you want the freelancers' information to be displayed on the page?
+    // Have you written and called a function to render the initial freelancer data?
+    // Have you written a function to generate a new random freelancer?
+    // Is this function being called in an interval?
+    // Have you written a function to calculate the average starting price of your freelancers' array?
+    // When should this function be called to update the displayed message?
